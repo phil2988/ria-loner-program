67d0641598ba502d659146aa8c7f0769337e9fe7
phil2988 <phillipblicher@gmail.com>
2023-05-10 07:34:35 +0200

    scrollable now

--- lib/main.dart
+++ lib/main.dart
@@ -50,85 +50,85 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('RIA Udlåns program'),
       ),
-      body: Center(
-        child: FutureBuilder(
-            future: loadJsonString(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                var data = jsonDecode(snapshot.data as String);
-                List<DataColumn> columns = [
-                  const DataColumn(
-                    label: Text("Loaner"),
-                  ),
-                  const DataColumn(label: Text("Study number")),
-                  const DataColumn(label: Text("Employee")),
-                  const DataColumn(label: Text("Comments")),
-                  const DataColumn(label: Text("Loan Date")),
-                  const DataColumn(
-                    label: Text("Return Date"),
-                  ),
-                  const DataColumn(label: Text("Items")),
-                ];
+      body: FutureBuilder(
+          future: loadJsonString(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var data = jsonDecode(snapshot.data as String);
+              List<DataColumn> columns = [
+                const DataColumn(
+                  label: Text("Loaner"),
+                ),
+                const DataColumn(label: Text("Study number")),
+                const DataColumn(label: Text("Employee")),
+                const DataColumn(label: Text("Comments")),
+                const DataColumn(label: Text("Loan Date")),
+                const DataColumn(
+                  label: Text("Return Date"),
+                ),
+                const DataColumn(label: Text("Items")),
+              ];
 
-                List<DataRow> rows = data
-                    .map<DataRow>(
-                      (e) => DataRow(
-                        color: DateTime.parse(e["returnDate"])
-                                    .compareTo(DateTime.now()) <
-                                0
-                            ? MaterialStateProperty.all(Colors.red)
-                            : null,
-                        onSelectChanged: (value) {},
-                        cells: [
-                          DataCell(Text(e["loaner"])),
-                          DataCell(Text(e["studyNumber"])),
-                          DataCell(Text(e["employee"])),
-                          DataCell(Text(e["comments"])),
-                          DataCell(Text(
-                              "${DateTime.parse(e["loanDate"]).day}/${DateTime.parse(e["loanDate"]).month}-${DateTime.parse(e["loanDate"]).year}")),
-                          DataCell(Text(
-                              "${DateTime.parse(e["returnDate"]).day}/${DateTime.parse(e["returnDate"]).month}-${DateTime.parse(e["returnDate"]).year}")),
-                          DataCell(
-                            Text((e["items"] as List<dynamic>)
-                                .map((e) =>
-                                    e["name"] +
-                                    ": " +
-                                    e["category"].toString().trim())
-                                .toList()
-                                .toString()
-                                .replaceAll("[", "")
-                                .replaceAll("]", "")
-                                .replaceAll(", ", "\n")),
-                          )
-                        ],
-                      ),
-                    )
-                    .toList();
+              List<DataRow> rows = data
+                  .map<DataRow>(
+                    (e) => DataRow(
+                      color: DateTime.parse(e["returnDate"])
+                                  .compareTo(DateTime.now()) <
+                              0
+                          ? MaterialStateProperty.all(Colors.red)
+                          : null,
+                      onSelectChanged: (value) {},
+                      cells: [
+                        DataCell(Text(e["loaner"])),
+                        DataCell(Text(e["studyNumber"])),
+                        DataCell(Text(e["employee"])),
+                        DataCell(Text(e["comments"])),
+                        DataCell(Text(
+                            "${DateTime.parse(e["loanDate"]).day}/${DateTime.parse(e["loanDate"]).month}-${DateTime.parse(e["loanDate"]).year}")),
+                        DataCell(Text(
+                            "${DateTime.parse(e["returnDate"]).day}/${DateTime.parse(e["returnDate"]).month}-${DateTime.parse(e["returnDate"]).year}")),
+                        DataCell(
+                          Text((e["items"] as List<dynamic>)
+                              .map((e) =>
+                                  e["name"] +
+                                  ": " +
+                                  e["category"].toString().trim())
+                              .toList()
+                              .toString()
+                              .replaceAll("[", "")
+                              .replaceAll("]", "")
+                              .replaceAll(", ", "\n")),
+                        )
+                      ],
+                    ),
+                  )
+                  .toList();
 
-                return SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: LayoutBuilder(builder: (context, constraints) {
-                    return Padding(
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: LayoutBuilder(builder: (context, constraints) {
+                  return SingleChildScrollView(
+                    child: Padding(
                       padding: EdgeInsets.symmetric(
                           horizontal: constraints.maxWidth * 0.05,
-                          vertical: constraints.maxHeight * 0.1),
+                          vertical: constraints.maxHeight * 0.05),
                       child: DataTable(
                         columns: columns,
                         rows: rows,
                         dataRowHeight: 70,
                         showCheckboxColumn: false,
                       ),
-                    );
-                  }),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }),
-      ),
+                    ),
+                  );
+                }),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: addNewLoan,
         tooltip: 'Increment',
